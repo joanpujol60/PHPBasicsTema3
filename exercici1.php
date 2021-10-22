b109db90c8d8311d88e6708d7d509c72cb229ee0
Joan Pujol - php <joanpujol60@gmail.com>
2021-10-22 19:05:54 +0200

    Exercici 1

---
+++ exercici1.php
@@ -0,0 +1,18 @@
+<?php
+    $variableInteger=1234; // Integer
+    $variableDouble=1.23; // Double
+    $variableString="Cadena de caracteres"; // String
+    $variableBooleana=True; // Booleana
+    
+   
+
+
+    
+
+    echo 'Valor Integer: ' . $variableInteger . '<br>'; // Impresion de Integer
+    echo 'Valor Double: ' . $variableDouble . '<br>'; // Impresion de Double
+    echo 'Valor String: ' . $variableString . '<br>'; // Impresion de String
+    echo 'Valor Booleana true: ' . $variableBooleana . '<br>'; // Impresion de Booleana 
+    
+
+?>
