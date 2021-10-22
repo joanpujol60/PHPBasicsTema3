abd7f0f0f5293e577935778c39b269c8902a6689
Joan Pujol - php <joanpujol60@gmail.com>
2021-10-22 23:12:12 +0200

    Exercici 4

---
+++ exercici4.php
@@ -0,0 +1,35 @@
+<?php
+
+$x= 5;
+$y= 8;
+$n= 5.5;
+$m= 4.2;
+
+
+ 
+echo "Valor de x: ".$x. '<br>';
+echo "Valor de y: ".$y. '<br>';
+echo "Suma de x + y: ".$x+$y. '<br>';
+echo "Resta de x - y: ".$x-$y. '<br>';
+echo "Producte de x * y: ".$x*$y. '<br>';
+echo "Modul de x y: ".$x%$y. '<br>';
+echo '<br>';
+
+echo "Valor de n: ".$n. '<br>';
+echo "Valor de m: ".$m. '<br>';
+echo "Suma de n + m: ".$n+$m. '<br>';
+echo "Resta de n - m: ".$n-$m. '<br>';
+echo "Producte de n * m: ".$n*$m. '<br>';
+echo "Modul de n m: ".$n%$m. '<br>';
+echo '<br>';
+
+echo "Doble de x: ".$x*2 . '<br>';
+echo "Doble de y: ".$y*2 . '<br>';
+echo "Doble de n: ".$n*2 . '<br>';
+echo "Doble de m: ".$m*2 . '<br>';
+echo "Suma de x+ y + n + m: ".$x+$y+$n+$m. '<br>';
+echo "Producte de x * y * n * m: ".$x*$y*$n*$m. '<br>';
+echo '<br>';
+
+
+?>
