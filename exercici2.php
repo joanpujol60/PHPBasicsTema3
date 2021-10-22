637a0db98d3d5d3b5b05466ee692b6e2c02257d5
Joan Pujol - php <joanpujol60@gmail.com>
2021-10-22 19:23:11 +0200

    Exercici 2

---
+++ exercici2.php
@@ -0,0 +1,19 @@
+<?php
+
+    $variableString="Hello, World!";
+    echo $variableString . '<br>';
+    echo strtoupper($variableString) . ' - Convertida a Majuscules' . '<br>';
+    echo strlen($variableString) . ' - Longitud de la variable' . '<br>';
+    echo strrev($variableString) . ' - Variable invertida' . '<br>';
+    $segonavariable="Aquest és el curs de PHP";
+    echo $variableString . $segonavariable . ' - Concatenació de variables'
+    
+   
+
+
+    
+
+   
+    
+
+?>
