f289a5c118b982e309db0fa01f0b008433f3cc86
Joan Pujol - php <joanpujol60@gmail.com>
2021-10-25 17:15:59 +0200

    FCN1exercici6

---
+++ FCN1exercici6.php
@@ -0,0 +1,13 @@
+<?php
+    function isBitten() {
+      if (rand(0, 1) == 0){
+        echo "Charlie m'ha mossegat el dit";
+      }else{
+        echo "Charlie NO m'ha mossegat el dit";}
+        
+      }
+    
+    isBitten()
+    
+    ?>
+     
