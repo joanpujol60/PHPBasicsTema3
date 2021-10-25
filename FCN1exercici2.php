7f4e94577b6ae9355e8d45dd0885352ce5ad26fd
Joan Pujol - php <joanpujol60@gmail.com>
2021-10-25 16:19:58 +0200

    FCN1exercici2

---
+++ FCN1exercici2.php
@@ -0,0 +1,12 @@
+<?php
+    function amagatall() {
+        for ($cont=1 ; $cont<=10; $cont++){
+               if ($cont%2 == 0){
+                    echo $cont . '<br>';
+              }
+            }
+    }
+
+    amagatall();
+    
+    ?>
